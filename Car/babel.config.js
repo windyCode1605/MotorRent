cacd891935096efea9a9f176b90a5cc6df4f74f0
windyCode1605 <maiquangnguyenkt2004@gmail.com>
2025-04-29 00:17:45 +0700

    .

--- Car/babel.config.js
+++ Car/babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
     }]
-  ]
+  ],
 };
