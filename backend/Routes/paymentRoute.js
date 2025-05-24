0fae07a62ab70ae4e8a6be14a3c69b90fc66ba77
windyCode1605 <maiquangnguyenkt2004@gmail.com>
2025-05-24 15:34:29 +0700

    .

--- backend/Routes/paymentRoute.js
+++ backend/Routes/paymentRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const momoController = require('../controllers/paymentController');
 
 router.post('/create', momoController.createMoMoPayment);
+router.post('/momo-ipn', momoController.handleMomoIPN);
 
 module.exports = router;
