476218080e1a1bc78524fd6139f564042cbedc96
windyCode1605 <maiquangnguyenkt2004@gmail.com>
2025-05-25 23:37:04 +0700

    .

--- backend/Routes/maintenanceRoute.js
+++ backend/Routes/maintenanceRoute.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const maintenanceController = require('../controllers/maintenanceController');
 
 router.get('/', maintenanceController.getAllMaintenance);
+router.post('/create', maintenanceController.createMaintenance);
+router.put('/updateCost/:maintenance_id', maintenanceController.updateCost);
+
 
 module.exports = router;
