5009e2a54eb4e416808dc56bc058514001bbd66b
windyCode1605 <maiquangnguyenkt2004@gmail.com>
2025-05-25 23:37:24 +0700

    .

--- backend/Routes/serviceRoute.js
+++ backend/Routes/serviceRoute.js
@@ -5,5 +5,7 @@ const service = require('../controllers/serviceController');
 router.get('/get', service.getAllServices);
 router.post('/delete', service.deleteService);
 router.post('/create', service.createService);
+router.post('/update', service.updateService);
+router.get('/byMaintenance/:maintenance_id', service.getServicesByMaintenanceId);
 
 module.exports = router;
