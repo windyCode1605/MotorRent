1415492fce31b7ff468e87432a63ccd316252a3c
windyCode1605 <maiquangnguyenkt2004@gmail.com>
2025-05-29 17:45:59 +0700

    .

--- backend/Routes/customerRoutes.js
+++ backend/Routes/customerRoutes.js
@@ -8,19 +8,11 @@ const {
     deleteCustomer
 } = require('../controllers/customerController');
 
-// Get all customers
+
 router.get('/', getAllCustomers);
-
-// Get customer by account ID
 router.get('/account/:account_id', getCustomerByAccountId);
-
-// Get customer by ID
 router.get('/:customer_id', getCustomerById);
-
-// Update customer
 router.put('/:customer_id', updateCustomer);
-
-// Delete customer
 router.delete('/:customer_id', deleteCustomer);
 
 
